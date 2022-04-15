248ee7ad64d3d87553645b8ca0b1cc6c339bed8e
HiiiXinyiii <1159736359@qq.com>
2022-04-15 23:30:42 +0800

    fix bugs

--- miniprogram/pages/teamCreate/teamCreate.js
+++ miniprogram/pages/teamCreate/teamCreate.js
@@ -8,17 +8,16 @@ Page({
         isSubmit: false,
         warn: "",
         phone: "",
-        pwd: "",
-        isPub: false,
-        sex: "男",
+        sex: "",
         start_date: "",      // 车队出发的日期
         start_time: "",      // 车队出发的时间
         end_date: "",        // 车队到达的日期
         end_time: "",         // 车队到达的时间
-        start_addr: "",     //出发地址
+        start_addr: "",       //出发地址
         start_locationName: "", //出发地址名称
         des_addr: "", //终点地址
-        des_locationName: "" //终点地址名称
+        des_locationName: "",    //终点地址名称
+        note: ""            // 备注信息
     },
     
     onShow: function () {
@@ -63,6 +62,8 @@ Page({
         chooseDesLocation=null;
         chooseStartLocation=requirePlugin('chooseLocation');
     },
+
+    // 
     showDesMap() {
         //使用在腾讯位置服务申请的key（必填）
         const key = "I5OBZ-GGG6U-YY2VL-2MX37-5ADG3-7QFJZ"; 
@@ -74,63 +75,123 @@ Page({
         chooseStartLocation=null;
         chooseDesLocation=requirePlugin('chooseLocation');
     },
+
+        // 选择出发\到达的时间\日期的变化
+        bindDatetimeChange: function(e) {
+            /******
+             * 函数功能：选择出发\到达的时间\日期的变化
+             * 函数参数：e.target.dataset.op_type取自[start_date, start_time, end_date, end_time]
+             *****/
+            switch(e.target.dataset.op_type) {
+                case "start_date":
+                    this.setData({
+                        start_date: e.detail.value
+                    })
+                    break;
+                case "start_time":
+                    this.setData({
+                        start_time: e.detail.value
+                    })
+                    break;
+                case "end_date":
+                    this.setData({
+                        end_date: e.detail.value
+                    })
+                    break;
+                case "end_time":
+                    this.setData({
+                        end_time: e.detail.value
+                    })
+                    break;
+                default:
+                    console.log("Wrong Operation! Please make sure that the parameter is among [start_date, start_time, end_date, end_time]");
+                    break;
+           };
+        },
+
+    // 获取文本框输入
+    bindTextAreaBlur: function(e) {
+    	this.setData({
+    	  note: e.detail.value
+    	}) 
+    },    
+
+
     // 检查提交的表单的合法性
     formSubmit: function (e) {
-        console.log('form发生了submit事件，携带数据为：', e.detail.value);
-        let { phone, pwd, isPub, sex } = e.detail.value;
-        if (!phone || !pwd) {
-          this.setData({
-            warn: "手机号或密码为空！",
-            isSubmit: true
-          })
-          return;
+        // 获得结果
+        let { teamname, phone, sex } = e.detail.value;  
+        // 提示信息
+        var checker = "";
+        checker = !teamname ? checker + "队伍名字为空 " : checker;  // 没有填写队名
+        checker = phone.length!=11 ? checker + "手机号码不规范 " : checker; // 手机号码位数不对
+        checker = !sex ? checker + "未选择性别 " : checker; 
+        checker = this.data.start_date == "" ? checker + "未选择出发日期 " : checker;
+        checker = this.data.start_time == "" ? checker + "未选择出发时间 " : checker;
+        checker = this.data.end_date == "" ? checker + "未选择到达日期 " : checker;
+        checker = this.data.end_time == "" ? checker + "未选择到达时间 " : checker;
+        checker = this.data.start_addr == "" ? checker + "未选择出发地 " : checker;
+        checker = this.data.des_addr == "" ? checker + "未选择目的地 " : checker;
+
+        // 检查是否有错误信息
+        // 有报错信息
+        if (checker != ""){
+            this.setData({
+                warn: checker,
+                isSubmit: checker=="" ? true : false,
+                phone: "",
+                sex: ""
+            });
+            // 弹窗提示
+            wx.showToast({
+                title: checker,
+                icon: 'none',
+                duration: 2000  // 持续的时间
+            });
         }
-        this.setData({
-          warn: "",
-          isSubmit: true,
-          phone,
-          pwd,
-          isPub,
-          sex
-        })
+        // 没有报错信息
+        else{
+            this.setData({
+                warn: checker,
+                isSubmit: checker=="" ? true : false,
+                phone: phone,
+                sex: sex
+            });
+            // 弹窗提示成功
+            wx.showToast({
+                title: '创建成功',
+                icon: 'none',
+                duration: 1500,
+                success: function () {
+                    // 弹窗后执行，可以省略
+                    setTimeout(function () {
+                        // 跳转到行程详细界面
+                        wx.navigateTo({
+                            url: "/pages/personalInfo/history/historyInfo",
+                        })
+                    }, 1500);
+                }
+            });
+        }
     },
 
     // 重置表单的输入
     formReset: function () {
-      console.log('form发生了reset事件')
-    },
-
-    // 选择出发\到达的时间\日期的变化
-    bindDatetimeChange: function(e) {
-        /******
-         * 函数功能：选择出发\到达的时间\日期的变化
-         * 函数参数：e.target.dataset.op_type取自[start_date, start_time, end_date, end_time]
-         *****/
-        switch(e.target.dataset.op_type) {
-            case "start_date":
-                this.setData({
-                    start_date: e.detail.value
-                })
-                break;
-            case "start_time":
-                this.setData({
-                    start_time: e.detail.value
-                })
-                break;
-            case "end_date":
-                this.setData({
-                    end_date: e.detail.value
-                })
-                break;
-            case "end_time":
-                this.setData({
-                    end_time: e.detail.value
-                })
-                break;
-            default:
-                console.log("Wrong Operation! Please make sure that the parameter is among [start_date, start_time, end_date, end_time]");
-                break;
-       };
+        this.setData({
+            isSubmit: false,
+            warn: "",
+            phone: "",
+            sex: "",
+            start_date: "",      // 车队出发的日期
+            start_time: "",      // 车队出发的时间
+            end_date: "",        // 车队到达的日期
+            end_time: "",         // 车队到达的时间
+            start_addr: "",       //出发地址
+            start_locationName: "", //出发地址名称
+            des_addr: "", //终点地址
+            des_locationName: "",    //终点地址名称
+            note: ""            // 备注信息
+        });
+        this.onReady();
     }
-
 })
