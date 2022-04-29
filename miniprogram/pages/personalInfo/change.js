7ab1947c6e0da7212905fab01bf3101b2e871120
unknown <unknown@example.com>
2022-04-29 22:57:20 +0800

    完善用户信息修改后端功能

--- miniprogram/pages/personalInfo/change.js
+++ miniprogram/pages/personalInfo/change.js
@@ -1,30 +1,60 @@
 // pages/personalinfo/change.js
+var app=getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    placeholder: '', //输入提示的显示信息
-    //相当于根据变量决定提示语
+    //输入提示的显示信息
+    placeholder: '',
+    //根据变量决定提示语
     infoArray:{
       name:'用户名',
+      gender:'性别',
+      phone:'手机号',
+      email: '邮箱'
       //其他信息，可根据需要加
     },
-
-    tmp:''    //用于存新修改的信息
+    changeWhat: '', //修改信息的参数
+    tmp: ''    //用于存新修改的信息
   },
 
+  //将新输入的值赋给tmp
   valuechange(res){
     this.setData({
       tmp: res.detail.value
     })
   },
 
-  submit:function(res){
-    //用于测试，后期应当写入数据库，并在下方加入返回前一个页面的函数
-    wx.setNavigationBarTitle({
-      title: this.data.tmp,
+  //提交数据库
+  submit:function(){
+    //写入数据库，并在下方加入返回前一个页面的函数
+    var that=this;
+    if(that.data.changeWhat == 'gender'){
+      if(that.data.tmp =='男')
+        that.data.tmp = 0;
+      else
+        that.data.tmp = 1;
+    }
+    //修改用户对应信息
+    wx.request({
+      method:'POST',
+      data:{
+        'option': that.data.changeWhat,
+        'useropenid': app.globalData.userInfo.useropenid,
+        'value': that.data.tmp
+      },
+      url: 'http://124.71.160.151:3006/modifyuserinfo',
+      success:function(res){
+        console.log(res.data)
+        wx.showToast({
+          title: '修改成功',
+        })
+        setTimeout(()=>{
+          wx.navigateBack()
+        }, 1000)
+      }
     })
   },
 
@@ -34,7 +64,8 @@ Page({
    */
   onLoad(options) {
     this.setData({
-      placeholder: '请输入' + this.data.infoArray[options.changeWhat]
+      placeholder: '请输入' + this.data.infoArray[options.changeWhat],
+      changeWhat: options.changeWhat
     })
     wx.setNavigationBarTitle({
       title: '修改' + this.data.infoArray[options.changeWhat],
