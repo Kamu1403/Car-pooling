74b861ca1443cbc07a125c4c67343e7e52b00c4e
MuCheng610 <2569219973@qq.com>
2022-06-16 15:01:46 +0800

    添加了邮箱正则匹配、用户名修改长度限制

--- miniprogram/pages/personalInfo/change.js
+++ miniprogram/pages/personalInfo/change.js
@@ -1,5 +1,5 @@
 // pages/personalinfo/change.js
-var app=getApp();
+var app = getApp();
 Page({
 
   /**
@@ -9,10 +9,10 @@ Page({
     //输入提示的显示信息
     placeholder: '',
     //根据变量决定提示语
-    infoArray:{
-      name:'用户名',
-      gender:'性别',
-      phone:'手机号',
+    infoArray: {
+      name: '用户名',
+      gender: '性别',
+      phone: '手机号',
       email: '邮箱'
       //其他信息，可根据需要加
     },
@@ -21,22 +21,22 @@ Page({
   },
 
   //将新输入的值赋给tmp
-  valuechange(res){
+  valuechange(res) {
     this.setData({
       tmp: res.detail.value
     })
   },
 
   //提交数据库
-  submit:function(){
+  submit: function () {
     //写入数据库，并在下方加入返回前一个页面的函数
-    var that=this;
-    if(that.data.changeWhat == 'gender'){
-      if(that.data.tmp =='男')
+    var that = this;
+    if (that.data.changeWhat == 'gender') {
+      if (that.data.tmp == '男')
         that.data.tmp = 0;
-      else if(that.data.tmp =='女')
+      else if (that.data.tmp == '女')
         that.data.tmp = 1;
-      else{
+      else {
         wx.showToast({
           icon: 'error',
           title: '请输入男或女'
@@ -44,8 +44,8 @@ Page({
         return;
       }
     }
-    else if(that.data.changeWhat == 'phone'){
-      if(that.data.tmp.length != 11 || isNaN(Number(that.data.tmp,10))){
+    else if (that.data.changeWhat == 'phone') {
+      if (that.data.tmp.length != 11 || isNaN(Number(that.data.tmp, 10))) {
         wx.showToast({
           icon: 'none',
           title: '请输入11位手机号，不允许有除数字外的字符'
@@ -53,22 +53,44 @@ Page({
         return;
       }
     }
+    else if (that.data.changeWhat == 'email') {
+      var email = that.data.tmp;
+      var reg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+      if (reg.test(email)) {
+        console.log("邮箱格式正确");
+      } else {
+        wx.showToast({
+          icon: 'none',
+          title: '邮箱格式不正确，请重新输入！'
+        })
+        return;
+      }
+    }
+    else if(that.data.changeWhat == 'name'){
+      if (that.data.tmp.length >= 20) {
+        wx.showToast({
+          icon: 'none',
+          title: '用户名长度不可以超过20！'
+        })
+        return;
+      }
+    }
 
     //修改用户对应信息
     wx.request({
-      method:'POST',
-      data:{
+      method: 'POST',
+      data: {
         'option': that.data.changeWhat,
         'useropenid': app.globalData.userInfo.useropenid,
         'value': that.data.tmp
       },
       url: 'http://124.71.160.151:3006/modifyuserinfo',
-      success:function(res){
+      success: function (res) {
         console.log(res.data)
         wx.showToast({
           title: '修改成功',
         })
-        setTimeout(()=>{
+        setTimeout(() => {
           wx.navigateBack()
         }, 1000)
       }
